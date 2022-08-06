0d58bb6ae08a167c99ac959736b0fa16a48d0ae6
klimiwe <klimiwe@mail.ru>
2022-08-06 12:03:39 +0400

    Third task

---
+++ Seminar/Hometasks/FirstSeminar/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+int number, remainder;
+Console.Write("Введите число: ");
+
+number = Convert.ToInt32(Console.ReadLine());
+
+remainder = number % 2;
+
+if (remainder == 0)
+{ Console.WriteLine("Число " + number + " является четным."); }
+
+else
+{ Console.WriteLine("Число " + number + " не является четным."); }
