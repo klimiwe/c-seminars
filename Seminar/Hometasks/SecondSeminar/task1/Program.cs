54e64c99bc1d1bd771acb4ac23a9583900a5649e
klimiwe <klimiwe@mail.ru>
2022-08-09 20:50:17 +0400

    Hometask task1

---
+++ Seminar/Hometasks/SecondSeminar/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98*/
+int numberWithoutSecondFigureOfNumber (int thirdDigitNumber)
+{
+    int firstFigure = thirdDigitNumber/100;
+    int thirtFigure = thirdDigitNumber%10;
+    int result = firstFigure*10+thirtFigure;
+    return result;
+}
+
+int thirdDigitNumber =  new Random (). Next (100, 999+1);
+Console.WriteLine ("Компьютер сгенерировал случайное трехзначное число: " + thirdDigitNumber);
+int result = numberWithoutSecondFigureOfNumber (thirdDigitNumber);
+Console.WriteLine ("Не понятно зачем компьютер выкинул вторую цифру из числа. И получилось: " + result);
