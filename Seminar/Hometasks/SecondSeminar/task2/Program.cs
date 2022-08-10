c1d3a0cdeb713de0badcce332a7a42d931bc7ccc
klimiwe <klimiwe@mail.ru>
2022-08-10 19:41:45 +0400

    Hometask task2 and3

---
+++ Seminar/Hometasks/SecondSeminar/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+
+int number =  new Random (). Next (1, 10000);
+Console.WriteLine ("Компьютер сгенерировал случайное число: " + number);
+int thirdFigure;
+
+if (number>99)
+{
+     while (number>=999)
+     {
+        
+        number = number/10;
+     }
+     Console.WriteLine("Третья цифра в числе "  +" :" + number%10);
+}
+else 
+{
+    Console.WriteLine ("В числе нет третьей цифры");
+}
+
---
+++ Seminar/Hometasks/SecondSeminar/task3/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+string nameOfTheDay(int numberOfTheDay)
+{
+    string result;
+    if (numberOfTheDay == 1)
+    {
+        result = "понедельник";
+    }
+else if (numberOfTheDay == 2)
+    {
+        result = "вторник";
+    }
+    else if (numberOfTheDay == 3)
+    {
+        result = "среда";
+    }
+    else if (numberOfTheDay == 4)
+    {
+        result = "четверг";
+    }
+    else if (numberOfTheDay == 5)
+    {
+        result = "пятница";
+    }
+    else if (numberOfTheDay == 6)
+    {
+        result = "суббота";
+    }
+    else 
+    {
+        result = "воскресенье";
+    }
+    return result;
+    
+}
+
+Console.WriteLine ("Введите номер дня недели.");
+int numberOfTheDay = Convert.ToInt32(Console.ReadLine());
+string name;
+if (numberOfTheDay > 0 & numberOfTheDay < 8)
+{
+    name = nameOfTheDay(numberOfTheDay);
+    Console.WriteLine("Сегодня " + name);
+}
+else 
+{
+    Console.WriteLine("Такого дня не существует");
+}
